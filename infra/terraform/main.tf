c0123129eb9e73b6a9b770a194d0a66802ba2830
NarimenB <nariboum@icloud.com>
2026-07-01 14:03:44 +0200

    feat: add ansible roles and playbooks for container configuration

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,4 +1,3 @@
-# Configuration Terraform
 terraform {
   required_version = ">= 1.6"
   required_providers {
@@ -9,29 +8,32 @@ terraform {
   }
 }
 
-# Provider Docker (local)
 provider "docker" {}
 
-# --- Réseau ---
 resource "docker_network" "app_network" {
-  name = "devops-network"
+  name = "devops-dev"
 }
 
-# --- Image Nginx ---
 resource "docker_image" "nginx" {
   name         = "nginx:alpine"
   keep_locally = true
 }
 
-# --- Conteneur Nginx ---
+resource "docker_image" "postgres" {
+  name         = "postgres:15-alpine"
+  keep_locally = true
+}
+
 resource "docker_container" "web" {
-  name  = "${var.app_name}-${var.environment}"
+  count = 2
+  name  = "${var.app_name}-${var.environment}-${count.index}"
   image = docker_image.nginx.image_id
 
   ports {
     internal = 80
-    external = var.web_port
+    external = 8080 + count.index
   }
+
   networks_advanced {
     name = docker_network.app_network.name
   }
@@ -42,14 +44,26 @@ resource "docker_container" "web" {
   ]
 }
 
-# --- Outputs ---
-output "web_url" {
-  value       = "http://localhost:${docker_container.web.ports[0].external}"
-  description = "URL du serveur web"
+resource "docker_container" "db" {
+  name  = "${var.app_name}-db-${var.environment}"
+  image = docker_image.postgres.image_id
+
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+
+  env = [
+    "POSTGRES_PASSWORD=secret123",
+    "POSTGRES_DB=devops"
+  ]
 }
 
-output "container_id" {
-  value = docker_container.web.id
+output "web_urls" {
+  value = [for c in docker_container.web : "http://localhost:${c.ports[0].external}"]
+}
+
+output "container_names" {
+  value = [for c in docker_container.web : c.name]
 }
 
 locals {
@@ -58,7 +72,6 @@ locals {
     environment = var.environment
     managed_by  = "terraform"
   }
-
   container_name = "${var.app_name}-${var.environment}"
 }
 
